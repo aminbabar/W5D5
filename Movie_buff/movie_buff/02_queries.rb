827556bc94fd7a38eae6128308e93d6678c86344
aminbabar <amin.babar1@gmail.com>
2021-07-16 16:10:21 -0400

    done with 2

--- Movie_buff/movie_buff/02_queries.rb
+++ Movie_buff/movie_buff/02_queries.rb
@@ -18,6 +18,11 @@ end
 def cast_list(title)
   # List all the actors for a particular movie, given the title.
   # Sort the results by starring order (ord). Show the actor id and name.
+  Actor
+    .select(:id, :name)
+    .joins(:movies)
+    .where("movies.title = ?", title)
+    .order("castings.ord ASC")
 
 end
 
@@ -28,10 +33,22 @@ def vanity_projects
 
   # Note: Directors appear in the 'actors' table.
 
+  Movie
+    .select("movies.id, movies.title, actors.name")
+    .joins(:actors)
+    .where("movies.director_id = castings.actor_id AND castings.ord = 1")
+
 end
 
 def most_supportive
   # Find the two actors with the largest number of non-starring roles.
   # Show each actor's id, name and number of supporting roles.
+  Actor
+    .select("actors.id, actors.name, COUNT(actors.id) AS roles")
+    .joins(:castings)
+    .where("castings.ord != 1")
+    .group("actors.id")
+    .order("COUNT(actors.id) DESC")
+    .limit(2)
 
 end
